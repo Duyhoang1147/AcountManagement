1b393f7a013bbcf6ed7a8d86b8aff616c22dbd91
Duy <Duyhoang1147@gmail.com>
2025-03-07 23:41:22 +0700

    Setup connect to mongoDB

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const conectDB = require('./database/mongoDB');
+
+const app = express();
 
 const PORT = 8080;
 
+conectDB();
+
 app.get('/', function (req, res) {
   res.send('Hello World')
-})
+});
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
