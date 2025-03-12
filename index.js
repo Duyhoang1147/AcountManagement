2eb916a680dfa6a367bd43f9d4afa2029be2f012
Duy <Duyhoang1147@gmail.com>
2025-03-13 02:18:21 +0700

    Add UI to test login

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 8080;
 
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'pubAlic')));
 
 app.use('/account', require('./routes/Account'));
 app.use('/auth', require('./routes/Auth') )
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'login.html'));
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'profile.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
